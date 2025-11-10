d9a10243ad93b8a2f60dc32b330c07f22309af01
EthanLourens-STUDENT <G00467054@atu.ie>
2025-11-10 16:38:28 +0000

    First

---
+++ Ethan.py
@@ -0,0 +1,23 @@
+import os
+import time 
+allowed_usernames = ["Ethan", "Franklin", "Daniel", "Sami", "Rio"]
+
+login_attempts = 0
+def scan_username():
+    global login_attempts
+
+while login_attempts < 3:
+    time.sleep(0.5)
+    os.system('cls')
+
+    username = input ("Enter Your Username: ")
+    login_attempts += 1
+
+    if username in allowed_usernames:
+        print(f"Welcome, {username}!")
+        print("You are in")
+        login_attempts = 0
+        break
+
+    else:
+        print("Invalid Username, Try Again")       
